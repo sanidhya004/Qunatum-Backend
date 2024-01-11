13eca507131d86ba7b8addeb7c357fb994c786d5
Sanidhya Sharma <sanidhya626@gmail.com>
2024-01-11 12:00:23 +0530

    frontend url added

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors(
  {
-  origin: ["http://127.0.0.1:5174"],
+  origin: [process.env.FRONTEND_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
  }
